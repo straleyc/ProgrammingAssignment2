86ffc4ec2e79ded2f22689337ae49c28d7e49cbb
BourneFan42 <BourneFan42@users.noreply.github.com>
2014-10-26 10:47:14 -0400

    Signed-off-by: BourneFan42 <BourneFan42@users.noreply.github.com>

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,56 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix and cacheSolve together create a special matrix 
+## which sets the value of the matrix, gets the value of the matrix 
+## (checks it), inverts the matrix, and caches the inverted matrix 
 
-## Write a short comment describing this function
+## makeCacheMatrix creates the special matrix and returns a list of fxns that: 
+## set the value of the matrix
+## get the value of the matrix
+## set the value of the inverse
+## get the value of the inverse 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL  # m will store the cached inverse matrix
+    
+    # function to set the matrix
+    set <- function(y) {
+        x<<- y
+        m <<- NULL
+    }
+    
+    # fxn to get the matrix 
+    get <- function() x
+    
+    # fxn to set the inverse 
+    setinvmat <- function(invmat) m <<- invmat
+    
+    # fxn to get the inverse
+    getinvmat  <- function() m
+    
+    # create list available to outside environments
+    list(set = set, get = get, setinvmat = setinvmat, getinvmat = getinvmat)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve:  check to see if the inverse matrix has
+## already been created.  If not, create is; if the 
+## inverse exists, return it
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    
+    ## put the value of the "getinvmat" subset of x into m
+    m <- x$getinvmat()
+    
+    ## if m already exists, return message, "Getting cached data", and return m 
+    if(!is.null(m)) {
+        message("Getting cached data")
+        return(m)
+    }
+    
+    ## if m DOESN'T exist, create the inverse matrix
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setinvmat(m)
+    
+    ## return the inverted matrix 
+    m
 }
